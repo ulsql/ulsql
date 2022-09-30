e400055e7bd066adc2e0bddce4f65cce70565259
Sugayuv1 <107237480+Sugayuv1@users.noreply.github.com>
2022-09-30 12:44:44 +0530

    Add files via upload

---
+++ install.sql
@@ -0,0 +1,6 @@
+@source/award_bonus/V1.01__employees_test.sql
+@source/award_bonus/R__award_bonus.prc
+@source/between_string/R__betwnstr.fnc
+@source/remove_rooms_by_name/V1.02__rooms.sql
+@source/remove_rooms_by_name/R__remove_rooms_by_name.prc
+exit
